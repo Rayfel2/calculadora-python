9c460059f5215cffb6d02310aa82d6b2141e825e
LoyaKnight <143142439+LoyaKnight@users.noreply.github.com>
2024-08-18 16:59:50 -0400

    Update dividir calculadora.py
    Agregue la opcion de dividir en la calculadora

--- calculadora.py
+++ calculadora.py
@@ -10,3 +10,9 @@ def Resta(a, b):
 
 def Mult(a, b):
     return a * b
+    
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero no permitida."
