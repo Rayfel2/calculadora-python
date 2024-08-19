5746389ff7c657b2d7761af5a650dd9e80bacd5c
Notengonombredisponible <106751866+Notengonombredisponible@users.noreply.github.com>
2024-08-19 18:53:31 -0400

    Funcion Factorial Agregada

--- calculadora.py
+++ calculadora.py
@@ -16,3 +16,10 @@ def dividir(a, b):
         return a / b
     else:
         return "Error: División por cero no permitida."
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("El factorial no está definido para números negativos")
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial_recursivo(n - 1)
